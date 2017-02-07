4d6406e5c0699c478bf6e548550655a63af87317
Claudio Caletti <claudio@buildo.io>
2017-02-07 21:36:28 +0100

    Add some comment

--- src/main/scala/Fico.scala
+++ src/main/scala/Fico.scala
@@ -1,5 +1,7 @@
 import org.scalatest._
 
+//Standard scala library for making typesafe-config nice
+//Same behavior as typesafe-config
 object Fico extends FunSuite with App {
   import com.typesafe.config._
   import net.ceedubs.ficus.Ficus._
@@ -9,4 +11,21 @@ object Fico extends FunSuite with App {
   assert(config.as[String]("service.http.host") == "0.0.0.0")
   assert(config.as[Int]("service.http.port") == 8080)
   assertThrows[ConfigException$Missing](config.as[String]("service.fico"))
+}
+
+//Knobs is a haskell-like library used by verizon
+//Ficus creator uses knobs
+object Knobbo extends FunSuite with App {
+  import com.typesafe.config._
+  import knobs.{ Required, ClassPathResource, Config }
+  import scalaz.concurrent.Task
+
+  val cfg: Task[Config] = knobs.Typesafe.config
+
+  val fico: Task[String] = for {
+    c <- cfg
+    f = c.require[String]("service.fico")
+  } yield f
+
+  assertThrows[knobs.KeyError](fico.run)
 }
